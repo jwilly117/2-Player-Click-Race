552c91e2635c94cc82bfad9ebb5c218b0ca76108
Jake Williams <jakewilliams117@gmail.com>
2020-02-10 18:53:48 -0500

    Adding psuedocode

---
+++ logic.js
@@ -0,0 +1,8 @@
+
+// 1. Create Necessary Firebase variables
+
+// 2. Logic for showing the values from firebase
+
+// 3. Logic for handling click events from player buttons
+
+// 4. Logic for handling reset button
